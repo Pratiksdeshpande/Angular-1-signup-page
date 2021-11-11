b86ebe50f30e65e132081311f2d187774ffe47b8
Pratik <pdeshpande666@gmail.com>
2021-11-11 14:57:08 +0530

    validation added

--- src/app/sign-up-page/sign-up-page.component.ts
+++ src/app/sign-up-page/sign-up-page.component.ts
@@ -7,16 +7,19 @@ import { FormGroup, FormControl, Validators } from "@angular/forms";
   styleUrls: ['./sign-up-page.component.css']
 })
 export class SignUpPageComponent {
+
+  vlaidEmail = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]){1,5}$/;
+
   userform = new FormGroup({
-    Firstname: new FormControl('',[Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
-    Lastname: new FormControl('',[Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
-    Email: new FormControl('',Validators.required),
-    password: new FormControl('',Validators.minLength(8)),
-    Phonenumber: new FormControl('',[Validators.required, Validators.pattern("^[0-9]*$")]),
-    terms: new FormControl('',Validators.requiredTrue)
+    Firstname: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
+    Lastname: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
+    Email: new FormControl('',Validators.pattern(this.vlaidEmail)),
+    password: new FormControl('', [Validators.minLength(8)]),
+    Phonenumber: new FormControl('', Validators.pattern("^([0-9]){10}$")),
+    terms: new FormControl('', Validators.requiredTrue)
   });
 
-  onSubmit(){
+  onSubmit() {
     console.log(this.userform);
   }
 
